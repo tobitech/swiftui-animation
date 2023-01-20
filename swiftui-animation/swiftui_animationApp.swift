77cbdbff6d4a1ff467c8c128466872df7adc0f92
Tobi Omotayo <tobitech@ymail.com>
2023-01-20 13:42:47 +0100

    Initial Commit

---
+++ swiftui-animation/swiftui_animationApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_animationApp.swift
+//  swiftui-animation
+//
+//  Created by Oluwatobi Omotayo on 20/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_animationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
