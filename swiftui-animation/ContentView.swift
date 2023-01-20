474604d308cc462defb4cae5f4640225378fd3a5
Tobi Omotayo <tobitech@ymail.com>
2023-01-20 13:58:10 +0100

    let's make a circle that we can drag around on the screen.
    first tag on a drag gesture and use the location value from the gesture to update an offset of the circle, then animate the changes with different kinds of animation provided

--- swiftui-animation/ContentView.swift
+++ swiftui-animation/ContentView.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+	
+	@State var circleCenter = CGPoint.zero
+	
 	var body: some View {
-		VStack {
-			Image(systemName: "globe")
-				.imageScale(.large)
-				.foregroundColor(.accentColor)
-			Text("Hello, world!")
-		}
-		.padding()
+		Circle()
+			.frame(width: 50, height: 50)
+			.offset(
+				x: self.circleCenter.x - 25, // to make it snap to the middle substract half of the diameter
+				y: self.circleCenter.y - 25
+			)
+			// .animation(.default)
+			// .animation(.linear)
+			// .animation(.easeIn)
+			.animation(.spring(response: 0.3, dampingFraction: 0.1))
+			.gesture(
+				DragGesture(minimumDistance: 0)
+					.onChanged { value in
+						self.circleCenter = value.location
+					}
+			)
 	}
 }
 
