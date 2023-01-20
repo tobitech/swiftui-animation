8e46786412c0d88af100f7b4637bbcfe32698f5b
Tobi Omotayo <tobitech@ymail.com>
2023-01-20 14:16:14 +0100

    add a fill colour animation

--- swiftui-animation/ContentView.swift
+++ swiftui-animation/ContentView.swift
@@ -10,11 +10,14 @@ import SwiftUI
 struct ContentView: View {
 	
 	@State var circleCenter = CGPoint.zero
+	@State var circleColor = Color.black
 	@State var isCircleScaled = false
 	
 	var body: some View {
 		VStack {
 			Circle()
+				.fill(self.circleColor)
+				.animation(.linear)
 				.frame(width: 50, height: 50)
 				.scaleEffect(self.isCircleScaled ? 2 : 1) // the order is important
 				.offset(
@@ -34,6 +37,28 @@ struct ContentView: View {
 			
 			Toggle("Scale", isOn: self.$isCircleScaled)
 				.padding(.horizontal)
+			Button("Circle Colors") {
+				// self.circleColor = .red
+				[Color.red, .blue, .green, .purple, .black]
+					.enumerated()
+					.forEach { offset, color in
+						DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(offset)) {
+							self.circleColor = color
+						}
+					}
+//				DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+//					self.circleColor = .blue
+//				}
+//				DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+//					self.circleColor = .green
+//				}
+//				DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+//					self.circleColor = .purple
+//				}
+//				DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
+//					self.circleColor = .black
+//				}
+			}
 		}
 	}
 }
