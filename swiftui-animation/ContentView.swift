1d53c5072c62839b2347c71591e892c9c094b0d8
Tobi Omotayo <tobitech@ymail.com>
2023-01-20 14:03:03 +0100

    add a scaling animation to the circle with a toggle to activate it

--- swiftui-animation/ContentView.swift
+++ swiftui-animation/ContentView.swift
@@ -10,24 +10,31 @@ import SwiftUI
 struct ContentView: View {
 	
 	@State var circleCenter = CGPoint.zero
+	@State var isCircleScaled = false
 	
 	var body: some View {
-		Circle()
-			.frame(width: 50, height: 50)
-			.offset(
-				x: self.circleCenter.x - 25, // to make it snap to the middle substract half of the diameter
-				y: self.circleCenter.y - 25
-			)
-			// .animation(.default)
-			// .animation(.linear)
-			// .animation(.easeIn)
-			.animation(.spring(response: 0.3, dampingFraction: 0.1))
-			.gesture(
-				DragGesture(minimumDistance: 0)
-					.onChanged { value in
-						self.circleCenter = value.location
-					}
-			)
+		VStack {
+			Circle()
+				.frame(width: 50, height: 50)
+				.scaleEffect(self.isCircleScaled ? 2 : 1) // the order is important
+				.offset(
+					x: self.circleCenter.x - 25, // to make it snap to the middle substract half of the diameter
+					y: self.circleCenter.y - 25
+				)
+				// .animation(.default)
+				// .animation(.linear)
+				// .animation(.easeIn)
+				.animation(.spring(response: 0.3, dampingFraction: 0.1))
+				.gesture(
+					DragGesture(minimumDistance: 0)
+						.onChanged { value in
+							self.circleCenter = value.location
+						}
+				)
+			
+			Toggle("Scale", isOn: self.$isCircleScaled)
+				.padding(.horizontal)
+		}
 	}
 }
 
